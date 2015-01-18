114929f8a1653fb9df7d89ed7f4f0c46885500e2
JJ <JJ@JJsMacBook.local>
2015-01-18 15:47:57 +0100

    first alpha for jambor.pro

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,54 +1,79 @@
 <?php 
 // No direct access
-defined('_JEXEC') or die; ?>
+defined('_JEXEC') or die;
+?>
 
-<h1>Contact</h1>
+<?php 
+if(isset($_POST['submit'])){
 
-<p>You can use this form to send an encrypted message. The form will generate the encrypted version of your message and you can send it right away.<p>
+	//echo '<script type="text/javascript">'
+	//	, 'encryptMessage();'
+	//	, '</script>'
+	//;
 
-<form>
-
-<input type="text" id="yourname" value="Your name"/>
-<input type="text" id="youremail" value="Your email address"/>
-<br/><br/>
-<textarea id="plaintext" name="plaintext" style="width: 100%; height: 150px">
-Your message.
-</textarea>
-<br/><br/>
-<input type="button" id="encryptmessage" name="encryptmessage" onclick="encryptMessage();" value="encrypt message">
-<br/><br/>
-<textarea name="encrypted" id="encrypted" style="width: 100%; height: 150px;">
-<?php echo $hello; ?>
-</textarea>
-<br/><br/>
-<input name="Submit" type="submit" value="Submit" />
-
-</form>
+	// see https://docs.joomla.org/Sending_email_from_extensions
+    $mailer = JFactory::getMailer();
+    $config = JFactory::getConfig();
+	$sender = array( 
+    	$config->get( 'config.mailfrom' ),
+    	$config->get( 'config.fromname' )
+    ); 
+	$mailer->setSender($sender);
+	$recipient = "website_contact@jambor.pro";
+	$mailer->addRecipient($recipient);
+	$body = $_POST['encrypted'];
+	$mailer->setSubject('Message from contact form jambor.pro');
+	$mailer->setBody($body);
+	$send = $mailer->Send();
+	if ( $send !== true ) {
+	    // echo 'Error sending email: ' . $send->__toString();
+	    echo '<p>Sorry your E-Mail was not send. We will try to fix that.</p>';
+	} else {
+    	echo '<p>Thanks for contacting me. I will reply as soon as possible.</p>';
+	}
+} else {
+	echo '<p>You can use this form to send an encrypted message to me. The form will generate the encrypted version of your message and you can send it right from this page.';
+	echo ' (This form is a self developed beta which makes use of <a href="https://keybase.io/kbpgp" target="_blank">keybas.io</a> Java Script implementation kbpgp.)</p>';
+	echo '<form  name="formencryptmessage" method="post">';
+	echo '	<input type="text" id="yourname" value="Your name" onClick="this.select();"/>';
+	echo '	<input type="text" id="youremail" value="Your email address" onClick="this.select();"/>';
+	echo '	<br/><br/>';
+	echo '	<textarea id="plaintext" name="plaintext" style="width: 100%; height: 150px" onClick="this.select();"> Write your message here.</textarea>';
+	echo '	<br/><br/>';
+	echo '	<input type="button" id="encryptmessage" name="encryptmessage" onclick="encryptMessage();" value="encrypt message">';
+	echo '	<br/><br/>';
+	echo '	<textarea name="encrypted" id="encrypted" style="width: 100%; height: 150px;" onClick="this.select();">Your encrypted message will be shown here after you click "encrypt message".</textarea>';
+	echo '	<br/><br/>';
+	echo '	<input name="submit" type="submit" value="Submit" />';
+	echo '</form>';
+}
+?>
 
 <script type="text/javascript" src="modules/mod_encryptedcontact/kbpgp/kbpgp-1.0.0.js"> </script>
 <script type="text/javascript">
 
-	function encryptMessage() {
+	function loadPublicKey() {
 		
 		var jamborjan;
 		var client = new XMLHttpRequest();
 		client.open('GET', 'https://keybase.io/jamborjan/key.asc');
 		client.onreadystatechange = function() {
 			jj_pgp_key = client.responseText;
+			console.log('jamborjan s key loaded');
 		};
 		client.send();
 		
-		kbpgp.KeyManager.import_from_armored_pgp({
-			armored: jj_pgp_key
-			}, function(err, jj) {
+	}
+
+	function encryptMessage() {
+		
+		kbpgp.KeyManager.import_from_armored_pgp({ armored: jj_pgp_key }, function(err, jj) {
   			if (!err) {
   				jamborjan = jj;
     			console.log('jamborjan is loaded');
   			}
 		});
 		
-// This is only a test
-
 		var params = {
   			encrypt_for: jamborjan,
   			msg:         'Name: '+document.getElementById('yourname').value+'\n'+'E-Mail: '+document.getElementById('youremail').value+'\n\n'+document.getElementById('plaintext').value
@@ -60,5 +85,7 @@ Your message.
 		});
 		
 	}
+
+	window.onload=loadPublicKey();
 	
 </script>
